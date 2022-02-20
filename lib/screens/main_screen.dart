9a6c54bf958be2c33ff030b85432624d1067f643
HSU WEI LUN <wayne90040@gmail.com>
2022-02-20 15:46:11 +0800

    🚸 feature-support keyboard in web

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_tertis/constants.dart';
 import 'package:flutter_tertis/controller/game_controller.dart';
 import 'package:flutter_tertis/controller/game_controller_roatate.dart';
@@ -15,82 +16,97 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    var gameController = Provider.of<GameController>(context, listen: false);
+    GameController gameController = Provider.of<GameController>(context, listen: false);
 
-    // TODO: implement build
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
+    return KeyboardListener(
+      autofocus: true,
+      focusNode: FocusNode(),
+      onKeyEvent: (event) {
+        if (event is KeyDownEvent || event is KeyRepeatEvent) {
+          if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+            gameController.moveRight();
+          }
+          else if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+            gameController.moveLeft();
+          }
+          else if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
+            gameController.rotatePiece();
+          }
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: <Widget>[
 
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: double.infinity,
-          child: Column(
-            children: <Widget>[
-
-              Container(
-                padding: const EdgeInsets.only(top: 20),
-                height: 50,
-                child: const Text("Tertis Game", 
-                    style: TextStyle(color: Colors.white)),
-              ),
-
-              SizedBox(
-                width: itemSize * column + column - 1,  // size + space
-                height: itemSize * row + row - 1,
-                child: Consumer<GameController>(
-                    builder: (context, controller, _) {
-                      return GridWidget(
-                        landedPieces: controller.landedPiecesColor,
-                        fallPieces: controller.fallingPiece,
-                        fallPiecesColor: controller.fallingPieceColor,
-                      );
-                    }
+                Container(
+                  padding: const EdgeInsets.only(top: 20),
+                  height: 50,
+                  child: const Text("TERTIS GAME",
+                      style: TextStyle(color: Colors.white)),
                 ),
-              ),
 
-              SizedBox(
-                width: itemSize * column + column - 1,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: IconButton(
-                        onPressed: () {
-                          gameController.startGame();
-                        },
-                        icon: const Icon(Icons.play_circle_fill, size: 50, color: Colors.white)
-                      ),
-                    ),
-                    Expanded(
-                      child: IconButton(
-                        onPressed: () {
-                          gameController.moveLeft();
-                        },
-                        icon: const Icon(Icons.arrow_left, size: 50, color: Colors.white)
-                      )
-                    ),
-                    Expanded(
-                      child: IconButton(
-                        onPressed: () {
-                          gameController.moveRight();
-                        },
-                        icon: const Icon(Icons.arrow_right, size: 50, color: Colors.white)
-                      ),
-                    ),
-                    Expanded(
-                      child: IconButton(
-                        onPressed: () {
-                          gameController.rotatePiece();
-                        },
-                        icon: const Icon(Icons.rotate_right, size: 50, color: Colors.white)
-                      )
-                    ),
-                  ]
+                SizedBox(
+                  width: itemSize * column + column - 1,  // size + space
+                  height: itemSize * row + row - 1,
+                  child: Consumer<GameController>(
+                      builder: (context, controller, _) {
+                        return GridWidget(
+                          landedPieces: controller.landedPiecesColor,
+                          fallPieces: controller.fallingPiece,
+                          fallPiecesColor: controller.fallingPieceColor,
+                        );
+                      }
+                  ),
                 ),
-              )
-            ],
+
+                SizedBox(
+                  width: itemSize * column + column - 1,
+                  child: Row(
+                      children: [
+                        Expanded(
+                          child: IconButton(
+                              onPressed: () {
+                                gameController.startGame();
+                              },
+                              icon: const Icon(Icons.play_circle_fill, size: 50, color: Colors.white)
+                          ),
+                        ),
+                        Expanded(
+                            child: IconButton(
+                                onPressed: () {
+                                  gameController.moveLeft();
+                                },
+                                icon: const Icon(Icons.arrow_left, size: 50, color: Colors.white)
+                            )
+                        ),
+                        Expanded(
+                          child: IconButton(
+                              onPressed: () {
+                                gameController.moveRight();
+                              },
+                              icon: const Icon(Icons.arrow_right, size: 50, color: Colors.white)
+                          ),
+                        ),
+                        Expanded(
+                            child: IconButton(
+                                onPressed: () {
+                                  gameController.rotatePiece();
+                                },
+                                icon: const Icon(Icons.rotate_right, size: 50, color: Colors.white)
+                            )
+                        ),
+                      ]
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 }
